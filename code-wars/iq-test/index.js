223e72f1fe1d534823fe01cc97b9ac82d3aab23e
imears <maxwellmears@gmail.com>
2017-03-30 21:15:29 -0700

    Add instructions to iq-test challenge

--- code-wars/iq-test/index.js
+++ code-wars/iq-test/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// Bob is preparing to pass IQ test. The most frequent task
+// in this test is to find out which one of the given numbers
+// differs from the others. Bob observed that one number usually
+// differs from the others in evenness. Help Bob — to check his
+// answers, he needs a program that among the given numbers finds
+// one that is different in evenness, and return a position of
+// this number.
+
+// ! Keep in mind that your task is to help Bob solve a real
+// IQ test, which means indexes of the elements start from 1 (not 0)
+
 module.exports = iqTest;
 
 function iqTest(_numbers){
